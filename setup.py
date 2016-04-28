8e93e3bf1f6b8eae6b49528a60d056bda9dc8bed
Yury Selivanov <yury@magic.io>
2016-04-28 00:18:37 -0400

    Bump version to 0.4.9

--- setup.py
+++ setup.py
@@ -53,7 +53,7 @@ class libuv_build_ext(build_ext):
 setup(
     name='uvloop',
     description='Fast implementation of asyncio event loop on top of libuv',
-    version='0.4.8',
+    version='0.4.9',
     packages=['uvloop'],
     cmdclass={'build_ext': libuv_build_ext},
     ext_modules=[
