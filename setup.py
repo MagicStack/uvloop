fbc8bb4248fa949c07097a95f655b43d0215a244
Yury Selivanov <yury@magic.io>
2016-05-13 11:24:38 -0400

    Bump version to v0.4.20

--- setup.py
+++ setup.py
@@ -96,7 +96,7 @@ setup(
     author='Yury Selivanov',
     author_email='yury@magic.io',
     platforms=['*nix'],
-    version='0.4.19',
+    version='0.4.20',
     packages=['uvloop'],
     cmdclass={'build_ext': libuv_build_ext},
     ext_modules=[
