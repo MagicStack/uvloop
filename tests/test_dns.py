011b1295eb7e948a8c11223b75e2eeae2bb744bb
Yury Selivanov <yury@magic.io>
2016-05-23 14:27:41 -0400

    tests: More tests for getaddrinfo()

--- tests/test_dns.py
+++ tests/test_dns.py
@@ -57,6 +57,7 @@ class BaseTestDNS:
 
     def test_getaddrinfo_1(self):
         self._test_getaddrinfo('example.com', 80)
+        self._test_getaddrinfo('example.com', 80, type=socket.SOCK_STREAM)
 
     def test_getaddrinfo_2(self):
         self._test_getaddrinfo('example.com', 80, flags=socket.AI_CANONNAME)
@@ -66,47 +67,63 @@ class BaseTestDNS:
 
     def test_getaddrinfo_4(self):
         self._test_getaddrinfo('example.com', 80, family=-1)
+        self._test_getaddrinfo('example.com', 80, type=socket.SOCK_STREAM,
+                               family=-1)
 
     def test_getaddrinfo_5(self):
         self._test_getaddrinfo('example.com', '80')
+        self._test_getaddrinfo('example.com', '80', type=socket.SOCK_STREAM)
 
     def test_getaddrinfo_6(self):
-        self._test_getaddrinfo(b'example.com', '80'.encode())
+        self._test_getaddrinfo(b'example.com', b'80')
+        self._test_getaddrinfo(b'example.com', b'80', type=socket.SOCK_STREAM)
 
     def test_getaddrinfo_7(self):
         self._test_getaddrinfo(None, 0)
+        self._test_getaddrinfo(None, 0, type=socket.SOCK_STREAM)
 
     def test_getaddrinfo_8(self):
         self._test_getaddrinfo('', 0)
+        self._test_getaddrinfo('', 0, type=socket.SOCK_STREAM)
 
     def test_getaddrinfo_9(self):
         self._test_getaddrinfo(b'', 0)
+        self._test_getaddrinfo(b'', 0, type=socket.SOCK_STREAM)
 
     def test_getaddrinfo_10(self):
         self._test_getaddrinfo(None, None)
+        self._test_getaddrinfo(None, None, type=socket.SOCK_STREAM)
 
     def test_getaddrinfo_11(self):
         self._test_getaddrinfo(b'example.com', '80')
+        self._test_getaddrinfo(b'example.com', '80', type=socket.SOCK_STREAM)
 
     def test_getaddrinfo_12(self):
-        self._test_getaddrinfo(b'example.com', b'80')
+        self._test_getaddrinfo('127.0.0.1', '80')
+        self._test_getaddrinfo('127.0.0.1', '80', type=socket.SOCK_STREAM)
 
     def test_getaddrinfo_13(self):
-        self._test_getaddrinfo('127.0.0.1', '80')
+        self._test_getaddrinfo(b'127.0.0.1', b'80')
+        self._test_getaddrinfo(b'127.0.0.1', b'80', type=socket.SOCK_STREAM)
 
     def test_getaddrinfo_14(self):
-        self._test_getaddrinfo(b'127.0.0.1', b'80')
+        self._test_getaddrinfo(b'127.0.0.1', b'http')
+        self._test_getaddrinfo(b'127.0.0.1', b'http', type=socket.SOCK_STREAM)
 
     def test_getaddrinfo_15(self):
-        self._test_getaddrinfo(b'127.0.0.1', b'http')
-
-    def test_getaddrinfo_16(self):
+        self._test_getaddrinfo('127.0.0.1', 'http')
         self._test_getaddrinfo('127.0.0.1', 'http', type=socket.SOCK_STREAM)
 
-    def test_getaddrinfo_17(self):
+    def test_getaddrinfo_16(self):
         self._test_getaddrinfo('localhost', 'http')
+        self._test_getaddrinfo('localhost', 'http', type=socket.SOCK_STREAM)
+
+    def test_getaddrinfo_17(self):
+        self._test_getaddrinfo(b'localhost', 'http')
+        self._test_getaddrinfo(b'localhost', 'http', type=socket.SOCK_STREAM)
 
     def test_getaddrinfo_18(self):
+        self._test_getaddrinfo(b'localhost', 'http')
         self._test_getaddrinfo(b'localhost', 'http', type=socket.SOCK_STREAM)
 
     ######
