3a3e4cc697f8f2d911676a17f2a33543a9445ab3
Pau Freixes <pau.freixes@skyscanner.net>
2018-04-17 07:39:07 +0200

    Fixed invalid pined expresion

--- .ci/travis-install.sh
+++ .ci/travis-install.sh
@@ -24,7 +24,7 @@ if [ "${TRAVIS_OS_NAME}" == "osx" ]; then
     # Pined to 9.0.X till following issues are addressed
     # https://github.com/pypa/pip/issues/5240
     # https://github.com/pyenv/pyenv/issues/1141
-    pip install --upgrade pip=~9.0.3
+    pip install --upgrade pip~=9.0.0
 else
     pip install --upgrade pip
 fi
