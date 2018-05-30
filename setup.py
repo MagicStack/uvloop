94e3a87063fbca6d3bd30f68f672f67c2b8048ba
Yury Selivanov <yury@magic.io>
2018-05-30 16:51:40 -0400

    setup: Add Python 3.7 classifier

--- setup.py
+++ setup.py
@@ -252,6 +252,7 @@ setup(
         'Programming Language :: Python :: 3 :: Only',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         'License :: OSI Approved :: Apache Software License',
         'License :: OSI Approved :: MIT License',
         'Intended Audience :: Developers',
