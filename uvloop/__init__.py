dc2256d04ab8519caec9c45ce41dd1c1a9a30136
Yury Selivanov <yury@magic.io>
2018-05-30 16:52:43 -0400

    Bump the dev version

--- uvloop/__init__.py
+++ uvloop/__init__.py
@@ -7,7 +7,7 @@ from . import _patch  # NOQA
 from .loop import Loop as __BaseLoop  # NOQA
 
 
-__version__ = '0.10.0'
+__version__ = '0.11.0.dev0'
 __all__ = ('new_event_loop', 'EventLoopPolicy')
 
 
