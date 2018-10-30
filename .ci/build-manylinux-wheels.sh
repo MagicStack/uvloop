b335b3c84bcc4f6d43cf2754f91692f68a4eb7fa
Yury Selivanov <yury@magic.io>
2018-10-30 18:52:40 -0400

    Fix CI building wheels

--- .ci/build-manylinux-wheels.sh
+++ .ci/build-manylinux-wheels.sh
@@ -5,8 +5,6 @@ set -e -x
 # Compile wheels
 PYTHON="/opt/python/${PYTHON_VERSION}/bin/python"
 PIP="/opt/python/${PYTHON_VERSION}/bin/pip"
-${PIP} install --upgrade pip wheel
-${PIP} install --upgrade setuptools
 ${PIP} install -r /io/.ci/requirements.txt
 make -C /io/ PYTHON="${PYTHON}" ci-clean compile
 ${PIP} wheel /io/ -w /io/dist/
