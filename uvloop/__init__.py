35f82506d19ef2602053304b36b93d06111d25af
Yury Selivanov <yury@edgedb.com>
2019-10-28 23:27:03 -0400

    Bump the version to 0.15.0.dev0

--- uvloop/__init__.py
+++ uvloop/__init__.py
@@ -7,7 +7,7 @@ from . import _patch  # NOQA
 from .loop import Loop as __BaseLoop  # NOQA
 
 
-__version__ = '0.14.0.dev0'
+__version__ = '0.15.0.dev0'
 __all__ = ('new_event_loop', 'install', 'EventLoopPolicy')
 
 
