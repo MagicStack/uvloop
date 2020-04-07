41a1ef81862259123cf134b1f3aca21760b8560a
Joongi Kim <joongi@lablup.com>
2020-04-07 13:12:08 +0900

    fix: Accept path-like objects in subprocess arguments
    * Also add subprocess test case using Path object
    
    * uvloop already handles path-like cwd correctly, so I just
      copy-and-pasted the same logic to _init_args() method.
    
    * The standard library uses "isinstance(obj, os.PathLike)" to check
      if an object is path-like, but os.PathLike exists as of Python 3.6.
      Since uvloop needs to support Python 3.5, we should use manual check
      for existence of the __fspath__ attribute.
    
    * According to the official Python documentation:
    
      - https://docs.python.org/3/library/subprocess.html#subprocess.Popen
        The subprocess.Popen() constructor support path-like objects since
        Python 3.6 on POSIX and since Python 3.8 on Windows.
    
      - https://docs.python.org/3/library/asyncio-eventloop.html#asyncio.loop.subprocess_exec
        This page does not mention about path-like objects,
        but as of Python 3.8, it DOES support path-like objects.

--- tests/test_process.py
+++ tests/test_process.py
@@ -135,6 +135,21 @@ class _TestProcess:
 
         self.loop.run_until_complete(test())
 
+    @unittest.skipIf(sys.version_info < (3, 8, 0),
+                     "3.5 to 3.7 does not support path-like objects "
+                     "in the asyncio subprocess API")
+    def test_process_executable_2(self):
+        async def test():
+            proc = await asyncio.create_subprocess_exec(
+                pathlib.Path(sys.executable),
+                b'-W', b'ignore', b'-c', b'print("spam")',
+                stdout=subprocess.PIPE)
+
+            out, err = await proc.communicate()
+            self.assertEqual(out, b'spam\n')
+
+        self.loop.run_until_complete(test())
+
     def test_process_pid_1(self):
         async def test():
             prog = '''\
