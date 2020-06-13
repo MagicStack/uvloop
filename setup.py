72f0877dd557496ac38089584e297651edd0615c
Naglis Jonaitis <naglis@mailbox.org>
2020-06-14 01:53:31 +0300

    setup: Add Python 3.8 classifier

--- setup.py
+++ setup.py
@@ -262,6 +262,7 @@ setup(
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
         'License :: OSI Approved :: Apache Software License',
         'License :: OSI Approved :: MIT License',
         'Intended Audience :: Developers',
