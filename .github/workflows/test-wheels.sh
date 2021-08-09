a4475e4f2e7b6267496896a68a92e6e39da81499
Elvis Pranskevichus <elvis@edgedb.com>
2021-08-08 17:12:44 -0700

    Use cibuildwheel to build wheels

--- .github/workflows/test-wheels.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-set -Eexuo pipefail
-shopt -s nullglob
-
-pip install -f "file:///${GITHUB_WORKSPACE}/dist" "uvloop[test]==${PKG_VERSION}"
-make -C "${GITHUB_WORKSPACE}" testinstalled
