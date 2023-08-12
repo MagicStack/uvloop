1526309a439a032c8a49fc197e20ec6fba92c867
Dulmandakh <dulmandakh@gmail.com>
2023-08-12 10:20:58 +0800

    exclude aiohttp from python 3.12

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ TEST_DEPENDENCIES = [
     # pycodestyle is a dependency of flake8, but it must be frozen because
     # their combination breaks too often
     # (example breakage: https://gitlab.com/pycqa/flake8/issues/427)
-    'aiohttp>=3.8.1',
+    'aiohttp>=3.8.1 ; python_version < "3.12"',
     'flake8~=5.0',
     'psutil',
     'pycodestyle~=2.9.0',
