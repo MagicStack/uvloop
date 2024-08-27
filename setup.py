7562c1ec85a87a9b3113c6617b46a04fbe4710f9
Fantix King <fantix.king@gmail.com>
2024-08-27 19:32:06 -0400

    Dorp unused env and only emit linenums in debug

--- setup.py
+++ setup.py
@@ -145,8 +145,8 @@ class uvloop_build_ext(build_ext):
                 self.distribution.ext_modules,
                 compiler_directives=directives,
                 annotate=self.cython_annotate,
-                compile_time_env=dict(DEFAULT_FREELIST_SIZE=250, SSL_READ_MAX_SIZE=256 * 1024),
-                emit_linenums=True)
+                compile_time_env=dict(DEFAULT_FREELIST_SIZE=250),
+                emit_linenums=self.debug)
 
         super().finalize_options()
 
